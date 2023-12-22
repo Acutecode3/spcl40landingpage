ae12c225ab47775d0d96ef59387203bfa9c27549
Alvin Varghese <alvin19official@gmail.com>
2023-12-22 15:08:16 +0530

    fix: mentor images data

--- src/lib/data/mentors.ts
+++ src/lib/data/mentors.ts
@@ -31,27 +31,35 @@ export const mentors = [
     "#DFD2B2"
   ),
   new Mentor(
-    "Arif Omar",
-    "Chartered Accountant",
+    "Adhil M",
+    "Certified Management Accountant",
     "/images/mentors/4.png",
     "#B2DFDC"
   ),
   new Mentor(
-    "Praveena Muralee C",
-    "Chartered Accountant",
+    "Vishnu Kadengal",
+    "CA Finalist",
     "/images/mentors/5.png",
     "#C0DFB2"
   ),
   new Mentor(
-    "Vishnu Kadengal",
-    "CA Finalist",
+    "Arif Omar",
+    "Chartered Accountant",
     "/images/mentors/6.png",
     "#DFD2B2"
   ),
   new Mentor(
-    "Adhil M",
-    "Certified Management Accountant",
-    "/images/mentors/6.png",
+    "Praveena Muralee C",
+    "Chartered Accountant",
+    "/images/mentors/7.png",
+    "#B2DFDC"
+  ),
+  new Mentor("Sajitha A S", "M. Com", "/images/mentors/8.png", "#C0DFB2"),
+  new Mentor("Divya CV", "M. Com", "/images/mentors/9.png", "#DFD2B2"),
+  new Mentor(
+    "Geethu Christy",
+    "MBA(Human Resource)",
+    "/images/mentors/10.png",
     "#B2DFDC"
   ),
 ];
